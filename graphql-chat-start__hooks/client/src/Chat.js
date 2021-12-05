1d26d4c79b951a97537cacdc45982a603ff33d50
Serhii Kokhan <serhii.kokhan5@gmail.com>
2021-12-05 18:17:59 +0100

    Created custom hooks for Apollo.

--- graphql-chat-start__hooks/client/src/Chat.js
+++ graphql-chat-start__hooks/client/src/Chat.js
@@ -1,35 +1,17 @@
-import React, { useState } from "react";
-import { useQuery, useMutation, useSubscription } from "@apollo/react-hooks";
-import {
-  addMessageMutation,
-  messageAddedSubscription,
-  messagesQuery,
-} from "./graphql/queries";
+import React from "react";
+import { useChatMessages } from "./hooks";
 import MessageInput from "./MessageInput";
 import MessageList from "./MessageList";
 
 const Chat = ({ user }) => {
-  const [messages, setMessages] = useState([]);
-  useQuery(messagesQuery, {
-    onCompleted: ({ messages }) => setMessages(messages),
-  });
-  useSubscription(messageAddedSubscription, {
-    onSubscriptionData: ({ subscriptionData }) => {
-      setMessages(messages.concat(subscriptionData.data.messageAdded));
-    },
-  });
-  const [addMessage] = useMutation(addMessageMutation);
-
-  const handleSend = async (text) => {
-    await addMessage({ variables: { input: { text } } });
-  };
+  const { messages, addMessage } = useChatMessages();
 
   return (
     <section className="section">
       <div className="container">
         <h1 className="title">Chatting as {user}</h1>
         <MessageList user={user} messages={messages} />
-        <MessageInput onSend={handleSend} />
+        <MessageInput onSend={addMessage} />
       </div>
     </section>
   );
